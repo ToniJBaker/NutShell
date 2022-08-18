b23e94a6442a527063a4fa0180894496d8f8510f
Jason Ellison <jason.w.ellison@gmail.com>
2022-08-18 03:41:20 -0400

    Sort events by date and highlight newest.

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -8,7 +8,7 @@
 
 
 
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState, useMemo } from "react"
 import { useNavigate } from "react-router-dom"
 import { fetchEvents, getLoggedInUser } from "../ApiManger"
 import { Event as AppEvent} from "./Event"
@@ -27,6 +27,14 @@ export const Events = () => {
             })
 
     }
+    const eventSort = (evt1, evt2) => {
+
+        const evt1Date = new Date(evt1.dateOf)
+        const evt2Date = new Date(evt2.dateOf)
+
+        return evt1Date - evt2Date
+
+    }
 
     useEffect(
         () => {
@@ -35,9 +43,13 @@ export const Events = () => {
         []
     )
 
+    const sortedEvents = useMemo(() => [...events].sort(eventSort), [events])
+
     return <>
             <button onClick={() => navigate("/event/create")}>Add Event</button>
             <h2 className="event-list__header">Events</h2>
-            {events.map((event) => <AppEvent key={`app-event--${event.id}`} event={event} />)}
+            <ul className="event-list">
+            {sortedEvents.map((event) => <li key={`app-event--${event.id}`}><AppEvent event={event} /></li>)}
+            </ul>
     </>
 }
